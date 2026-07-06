3ce7037fb8e642adb31aa5456993208a582dcf53
Lara Domingos Viana <laradomingos257@gmail.com>
2026-07-06 19:02:37 -0300

    falta crawler e espantalho

--- entities/spike.py
+++ entities/spike.py
@@ -2,6 +2,7 @@ import pygame
 from entities.entity import Entity
 from components.physics_body import PhysicsBody
 from components.hitbox import Hitbox
+from components.animation import AnimationController
 
 class Spike(Entity):
     def __init__(self, game, x, y, x_min, x_max, speed=200):
@@ -15,6 +16,11 @@ class Spike(Entity):
         self.x_max = x_max
         self.speed = speed
         self.dir   = 1  # começa indo para a direita
+
+        sheet = game.assets.image("assets/images/entities/spike_walk_sheet.png")
+        self.anim = AnimationController(self, sheet, frame_w=64, frame_h=24, fps=6)
+        self.anim.add("walk", row=0, start=0, end=1)
+        self.anim.play("walk")
 
     def update(self, dt):
         self.hb.tick(dt)
@@ -30,6 +36,8 @@ class Spike(Entity):
 
         self.vel.x = self.dir * self.speed
 
+        self.anim.play("walk", flip_x=(self.dir == -1))
+        self.anim.update(dt)
+
     def draw(self, surface, camera):
-        dr = camera.apply_rect(self.body.rect)
-        pygame.draw.rect(surface, (180, 50, 50), dr)  # vermelho escuro
+        self.anim.draw(surface, self.pos, camera)
