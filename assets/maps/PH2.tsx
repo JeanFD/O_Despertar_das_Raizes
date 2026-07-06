8f7004f21bc95c3806e1123637237d04de6ef0c0
Lara Domingos Viana <laradomingos257@gmail.com>
2026-07-05 21:04:27 -0300

    novo mapa

---
+++ assets/maps/PH2.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.12.1" name="PH2" tilewidth="32" tileheight="32" tilecount="4" columns="4">
+ <image source="../images/placeholder_tileset.png" width="128" height="32"/>
+</tileset>
